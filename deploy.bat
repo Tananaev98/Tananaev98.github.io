514b1eb8ba0d5505d6253289f9f5968c100a06d9
tda <dmit.tananaeff@yandex.ru>
2026-03-23 20:37:54 +0300

    quick update

--- deploy.bat
+++ deploy.bat
@@ -17,7 +17,7 @@ if errorlevel 1 (
     echo Нет изменений для коммита или ошибка коммита
 ) else (
     echo Отправляем на сервер...
-    git push origin master --force
+    git push origin master 
 )
 
 echo Done!
